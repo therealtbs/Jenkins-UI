957f26b366f07dcab23763f13c5746eb2f38ee55
therealtbs <therealtbs@gmail.com>
2014-04-19 18:13:02 +0200

    misc format + efficiency changes

--- ci.js
+++ ci.js
@@ -89,7 +89,7 @@ function getInfo(color) {
 
 }
 function getTableRows(builds, job, limit) {
-    limit =  typeof limit !== 'undefined' ?  limit : true;
+    limit = typeof limit !== 'undefined' ? limit : true;
     var html = "";
     var nr = 0;
     $.each(builds, function (i, build) {
@@ -120,33 +120,34 @@ function getTableClass(result) {
     }
 }
 function getDownloadButton(build, job) {
+    var html;
     if (build.result !== "SUCCESS") return 'No download links available';
     if (loadFromExternal && build.description === null) return 'No download links available';
 
     if (loadFromExternal && $.parseJSON(build.description).artifacts.length == 0) return 'No download links available';
     if (loadFromExternal) {
-        var html = $('<button>', {
+        html = $('<button>', {
             'class': 'btn btn-primary btn-xs',
             'onclick': 'showDownloadModal("' + job.displayName + '", "' + build.number + '", \'' + build.description + '\', "' + job.name + '")'
         }).text('Download links');
-        return html.wrap('<p>').parent().html();
+
     } else {
-        var html = $('<button>', {
+        html = $('<button>', {
             'class': 'btn btn-primary btn-xs',
             'onclick': 'showDownloadModal("' + job.displayName + '", "' + build.number + '", \'' + JSON.stringify(build.artifacts) + '\', "' + job.name + '")'
         }).text('Download links');
-        return html.wrap('<p>').parent().html();
-    }
 
+    }
+    return html.wrap('<p>').parent().html();
 
 }
 function showDownloadModal(displayName, number, artifacts, name) {
 
-    $('#download .modal-title').text('Download ' + displayName + ' Build #' + number);
+    $('#download').find('.modal-title').text('Download ' + displayName + ' Build #' + number);
     if (loadFromExternal) {
-        $('#download .modal-body').html(listDownloadLinks($.parseJSON(artifacts), name, number));
+        $('#download').find('.modal-body').html(listDownloadLinks($.parseJSON(artifacts), name, number));
     } else {
-        $('#download .modal-body').html(listDownloadLinks($.parseJSON(artifacts), name, number));
+        $('#').find('.modal-body').html(listDownloadLinks($.parseJSON(artifacts), name, number));
     }
     $('#download').modal()
 }
@@ -170,33 +171,33 @@ function getOverview() {
             dataType: "json",
             url: jenkinsPath + '/api/json?tree=jobs[name,color,url,description,displayName,lastSuccessfulBuild[number,description],builds[number,result,description,changeSet[items[msg]]]]',
             success: function (data) {
-            $('#content').empty();
+                $('#content').empty();
 
-            for (var i = 0; i < (data.jobs.length / 2); i++) {
-                $('#content').append('<div class="row"></div>')
-            }
-            data.jobs.forEach(function (i) {
-                var html = '<div class="col-lg-6"><div class="panel ' + getClass(i.color) + '">';
-                html += '<div class="panel-heading"><span class="panel-title"><a href="#!' + i.name + '">' + i.displayName + '</a></span><i data-toggle="tooltip" data-placement="top" title="' + getInfo(i.color) + '" class="fa fa-question-circle fa-lg pull-right"></i></div>';
-                html += '<div class=panel-body container"><div class="row"><div class="col-md-8">' + i.description + '</div><div class="col-md-4">';
-                if (i.lastSuccessfulBuild !== null && i.lastSuccessfulBuild.description !== null) {
-                    var artifacts = $.parseJSON(i.lastSuccessfulBuild.description).artifacts;
-
-                    artifacts.forEach(function (artifact) {
-                        html += '<a class="btn btn-primary" href="' + externalUri + i.name + '/' + i.lastSuccessfulBuild.number + '/' + artifact + '">Download <br class="visible-lg"/>' + artifact + '<br class="visible-lg"/> (latest)</a>';
-                    });
-
+                for (var i = 0; i < (data.jobs.length / 2); i++) {
+                    $('#content').append('<div class="row"></div>')
                 }
-                html += '</div></div><hr/></div><table class="table table-striped table-hover"><thead><tr><td>#</td><td>Changes</td><td>Download</td></tr></thead><tbody>' + getTableRows(i.builds, i) + '</tbody></table>';
-                html += '</div></div>';
-                $('#content .row').each(function (nr, i) {
-                    if ($(this).length != 2) {
-                        $(this).append(html);
-                        return false;
+                data.jobs.forEach(function (i) {
+                    var html = '<div class="col-lg-6"><div class="panel ' + getClass(i.color) + '">';
+                    html += '<div class="panel-heading"><span class="panel-title"><a href="#!' + i.name + '">' + i.displayName + '</a></span><i data-toggle="tooltip" data-placement="top" title="' + getInfo(i.color) + '" class="fa fa-question-circle fa-lg pull-right"></i></div>';
+                    html += '<div class=panel-body container"><div class="row"><div class="col-md-8">' + i.description + '</div><div class="col-md-4">';
+                    if (i.lastSuccessfulBuild !== null && i.lastSuccessfulBuild.description !== null) {
+                        var artifacts = $.parseJSON(i.lastSuccessfulBuild.description).artifacts;
+
+                        artifacts.forEach(function (artifact) {
+                            html += '<a class="btn btn-primary" href="' + externalUri + i.name + '/' + i.lastSuccessfulBuild.number + '/' + artifact + '">Download <br class="visible-lg"/>' + artifact + '<br class="visible-lg"/> (latest)</a>';
+                        });
+
                     }
+                    html += '</div></div><hr/></div><table class="table table-striped table-hover"><thead><tr><td>#</td><td>Changes</td><td>Download</td></tr></thead><tbody>' + getTableRows(i.builds, i) + '</tbody></table>';
+                    html += '</div></div>';
+                    $('#content').find('.row').each(function (nr, i) {
+                        if ($(this).length != 2) {
+                            $(this).append(html);
+                            return false;
+                        }
+                    });
                 });
-            });
-        },
+            },
             fail: function () {
                 $('#content').empty();
                 $('#content').append('<div class="alert alert-danger">Could not retrieve information</div>')
@@ -228,7 +229,7 @@ function getOverview() {
                     }
                     html += '</div></div><hr/></div><table class="table table-striped table-hover"><thead><tr><td>#</td><td>Changes</td><td>Download</td></tr></thead><tbody>' + getTableRows(i.builds, i) + '</tbody></table>';
                     html += '</div></div>';
-                    $('#content .row').each(function (nr, i) {
+                    $('#content').find('.row').each(function (nr, i) {
                         if ($(this).length != 2) {
                             $(this).append(html);
                             return false;
